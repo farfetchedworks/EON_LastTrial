b1fde5769eb660bb3b3bbd046adcbe090fe892ae
jxarco <alexroco.30@gmail.com>
2021-11-17 05:16:09 +0100

    ui fixes, allow MAX entities in world

--- Source/components/common/comp_culling.h
+++ Source/components/common/comp_culling.h
@@ -7,7 +7,7 @@
 #include <bitset>
 
 struct TCompCulling : public TCompBase {
-  static const size_t nBits = 8192;
+  static const size_t nBits = 16384;
   typedef std::bitset<nBits> TCullingBits;
   TCullingBits bits;
 
